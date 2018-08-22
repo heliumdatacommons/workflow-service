b4c091962eff88e358ae27d10ee5cb4e70727df2
Kyle Ferriter <kylerferriter@gmail.com>
2018-08-22 14:52:07 +0000

    capture server output

--- wes_service/run-wes.sh
+++ wes_service/run-wes.sh
@@ -3,4 +3,4 @@ wes-server --backend=wes_service.pivot_wes \
   --opt extra=--workDir=/toil-intermediate/workdir --opt extra=--batchSystem=chronos \
   --opt extra=--no-match-user --opt extra=--linkImports --opt extra=--not-strict \
   --opt extra=--jobStore=/toil-intermediate/jobstore-REPLACE \
-  --opt extra=--defaultCores=16 --opt extra=--defaultMemory=20G --opt extra=--defaultDisk=30G 
+  --opt extra=--defaultCores=16 --opt extra=--defaultMemory=20G --opt extra=--defaultDisk=30G 2>&1 | tee -a /tmp/wes.log
